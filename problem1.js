c176bdfc8d52b0b50c7c4928a8728998e9ced938
Neil Menezes <37837480+skoarkid@users.noreply.github.com>
2020-01-25 15:20:12 +0530

    Update problem1.js
    Improved time complexity

--- problem1.js
+++ problem1.js
@@ -1,20 +1,20 @@
 function multiplesOf3and5(number) {
-  var i;
-  var sum=0;
-  for(i=3;i<number;i++)
-  {
-    if(i%3==0)
-    {
-      sum += i;
-      continue;
-    }
-    if(i%5==0)
-    {
-      sum += i;
-      continue;
-    }
-  }
-  return sum;
+  var multipOf3 = getMultiples(number,3);
+  var multipOf5 = getMultiples(number,5);
+  var multipOf15 = getMultiples(number,15);
+  
+  return sum(multipOf3)*3 + sum(multipOf5)*5 - sum(multipOf15)*15;
+
+}
+
+function getMultiples(number,root)
+{
+  return (number%root==0) ? Math.floor(number/root) -1 : Math.floor(number/root);
+}
+
+function sum(number)
+{
+  return number*(number+1)/2
 }
 
 multiplesOf3and5(1000);
