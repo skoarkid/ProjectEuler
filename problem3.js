362dc055929d6ca0b9ffc3963f61114fe727c7cb
Neil Menezes <37837480+skoarkid@users.noreply.github.com>
2020-01-26 11:51:59 +0530

    Create problem3.js

---
+++ problem3.js
@@ -0,0 +1,33 @@
+function largestPrimeFactor(number) {
+  var factor = 2;
+  while(number>1)
+  {
+    if(isPrime(factor))
+    {
+      while(number%factor==0)
+      {
+        number/=factor;
+      }
+    }
+    if(number==1)
+    {
+      break;
+    }
+    factor++;
+  }
+  return factor;
+}
+
+function isPrime(number)
+{
+  for(var i=2;i<Math.floor(Math.sqrt(number))+1;i++)
+  {
+    if(number%i==0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+largestPrimeFactor(13195);
